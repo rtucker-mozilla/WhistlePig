f7ac409e924fc34ddc1540cbf2cbc7682e015d64
Rob Tucker <rtucker@mozilla.com>
2014-10-02 05:43:23 -0400

    Enforcing the rss private items as private

--- whistlepig/whistlepig/feeds.py
+++ whistlepig/whistlepig/feeds.py
@@ -1,16 +1,26 @@
 from django.contrib.syndication.views import Feed
 from models import StatusUpdate
+from .feed_views import Feed
 class LatestUpdatesFeed(Feed):
     title = "Latest Mozilla Notifications"
     link = "/rss/"
     description = "Updates on notifications and outages at Mozilla"
 
     def items(self):
-        return StatusUpdate.objects.order_by('-posted_on')[:5]
+        if self.is_anonymous:
+            return StatusUpdate.objects.filter(is_private=False).order_by('-posted_on')[:5]
+        else:
+            return StatusUpdate.objects.order_by('-posted_on')[:5]
 
     def item_title(self, item):
-        return item.summary
+        if self.is_anonymous and item.is_private:
+            return ''
+        else:
+            return item.summary
 
     def item_description(self, item):
-        return item.description
+        if self.is_anonymous and item.is_private:
+            return ''
+        else:
+            return item.description
 
